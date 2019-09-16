fa1a6321d92d21d62ff4b3116209a8574dac57bd
abalexandre <abalexandre@hotmail.com>
2019-09-16 13:43:49 -0400

    Initial android project commit

---
+++ Application_Carabin/app/src/main/java/com/example/application_carabin/BluetoothActivity.kt
@@ -0,0 +1,13 @@
+package com.example.application_carabin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+
+}
