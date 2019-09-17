0666611e7395b90a07202c72edb311a46f41bea9
vinceBo <vbonnardeaux@gmail.com>
2019-09-17 11:25:35 -0400

    Checking push

--- Application_Carabin/app/src/main/java/com/example/application_carabin/BluetoothActivity.kt
+++ Application_Carabin/app/src/main/java/com/example/application_carabin/BluetoothActivity.kt
@@ -8,6 +8,7 @@ class BluetoothActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bluetooth)
+        //Comment to check if push works
     }
 
 }
